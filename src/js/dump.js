e18f991f2deea95f0258e37b5b11063f5e302bd6
Sebastian 'Kuchen' Kucharczyk <kuchen@kekse.biz>
2024-11-04 16:05:12 +0100

    1730732710732468411

--- src/js/dump.js
+++ src/js/dump.js
@@ -4,7 +4,7 @@
  */
 
 //
-const DEFAULT_PARAM_SCHEME_JSON = 'json/param.json';
+const DEFAULT_PARAM_SCHEME_JSON = '../../json/param/dump.json';
 const DEFAULT_CONSOLE_WIDTH_MIN = 60;
 const DEFAULT_REFRESH = 1000;
 const DEFAULT_SILENT = true;
@@ -91,7 +91,7 @@ class Dump extends Quant
 			}
 			else if(!string(this.path = path, false))
 			{
-				console.error('Missing file path parameter.');
+				console.error('Missing file path parameter (and also no stdin input).');
 				return process.exit(true);
 			}
 			else if(this.path === '/dev/stdin')
@@ -243,17 +243,14 @@ class Dump extends Quant
 
 			//
 			this.print();
-		}, path.join(this.param.source, DEFAULT_PARAM_SCHEME_JSON), this.param);
+		}, path.join(this.param.script, DEFAULT_PARAM_SCHEME_JSON), this.param);
 	}
 
 	destroy(_name, _code, ... _args)
 	{
-		if(this.handle)
-		{
-			fs.closeSync(this.handle);
-		}
-
-		return super.destroy();
+		this.destroying = true;
+		if(this.handle) fs.closeSync(this.handle);
+		return super.destroy(... _args);
 	}
 	
 	get isFile()
