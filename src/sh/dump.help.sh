bf2cfc93e3fd7f6b917de05d71dfd49f503ee73d
Sebastian 'Kuchen' Kucharczyk <kuchen@kekse.biz>
2024-11-25 01:10:41 +0100

    1732493440241223155

---
+++ src/sh/dump.help.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# 
+# Copyright (c) Sebastian Kucharczyk <kuchen@kekse.biz>
+# https://norbert.com.es/
+# 
+
+#
+BASH="bash"
+BASH="`which $BASH 2>/dev/null`"
+
+if [[ $? -ne 0 ]]; then
+	echo "Your \`bash\` wasn't found!" >&2
+	exit 1
+fi
+
+#
+REAL="$(realpath "$0")"
+DIR="$(dirname "$REAL")"
+MAIN="$(realpath "${DIR}/dump.util.sh")"
+
+#
+CMD="${BASH} '${MAIN}' help"
+
+for i in "$@"; do
+	CMD="${CMD} '$i'"
+done
+
+#
+eval "$CMD"
+
