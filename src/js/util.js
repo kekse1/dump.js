3d7262f3fb621feae50e6297ca9df4df03d50f96
Sebastian 'Kuchen' Kucharczyk <kuchen@kekse.biz>
2024-12-13 12:53:26 +0100

    1734090805195023811

--- src/js/util.js
+++ src/js/util.js
@@ -371,7 +371,7 @@ class Utility extends Quant
 					if(this.high.has(this.counting[i][0]))
 					{
 						this.counting[i][1] = this.counting[i][1].bold(true).fg(255, 255, 255, true);
-						this.counting[i][2] = this.counting[i][2].bold(true).fg(255, 255, 255, true);
+						this.counting[i][2] = this.counting[i][2].fg(255, 255, 255, true);
 					}
 					else
 					{
