a7395e7412e08cdd577bac2cd8bf64e69776c0c7
Sebastian Kucharczyk <kuchen@kekse.biz>
2025-01-07 01:39:07 +0100

    1736210346109839230

--- src/sh/dump.printable.sh
+++ src/sh/dump.printable.sh
@@ -20,7 +20,7 @@ DIR="$(dirname "$REAL")"
 MAIN="$(realpath "${DIR}/dump.util.sh")"
 
 #
-CMD="${BASH} '${MAIN}' count --order yes --only '10,13,32,65-122' --high '10,13,32,65,97'"
+CMD="${BASH} '${MAIN}' count --order yes --only '10,13,32,65-122' --high '0-255+2'"
 
 for i in "$@"; do
 	CMD="${CMD} '$i'"
