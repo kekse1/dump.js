93438f7effbe49f340e9cc6828a899cb0c32189d
Sebastian 'Kuchen' Kucharczyk <kuchen@kekse.biz>
2025-01-23 00:21:07 +0100

    1737588066386841081

--- src/js/dump.js
+++ src/js/dump.js
@@ -805,23 +805,10 @@ class Dump extends Quant
 	}
 
 	//
-	//TODO/TEST this more, pls.!1
-	//
 	calculateColumns()
 	{
-		/*var columns = 0;
-		var length = this.lineBegin.textLength;
-
-		while((length + columns + 3 + this.radixDigits) <= (console.width || 80))
-		{
-			length += ((255).toString(this.radix) + ' ').length;
-			++columns;
-		}
-
-		return columns;*/
-
-		return Math._floor(
-			((console.width || 80) - this.lineBegin.textLength) /
+		return Math._floor(((console.width || 80) -
+			this.lineBegin.textLength) /
 				(this.radixDigits + 2));
 	}
 
