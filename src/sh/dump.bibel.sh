dac2ec152b8419262ebf818684d0f66908090d40
Sebastian 'Kuchen' Kucharczyk <kuchen@kekse.biz>
2025-06-27 00:51:02 +0200

    [1750978232] +

---
+++ src/sh/dump.bibel.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# 
+# Copyright (c) Sebastian Kucharczyk <kuchen@kekse.biz>
+# https://norbert.com.es/
+# 
+
+#
+BASH="bash"
+BASH="`which $BASH 2>/dev/null`"
+
+if [[ $? -ne 0 ]]; then
+	echo "Your \`bash\` wasn't found!" >&2
+	exit 1
+fi
+
+#
+REAL="$(realpath "$0")"
+DIR="$(dirname "$REAL")"
+MAIN="$(realpath "${DIR}/dump.util.sh")"
+
+#
+CMD="${BASH} '${MAIN}' count --sort yes --only '32-126,10,13' --high '32,101,110,105,114,97,115,100,116,104'"
+
+for i in "$@"; do
+	CMD="${CMD} '$i'"
+done
+
+#
+eval "$CMD"
+
